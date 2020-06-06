74ca457920befe9eac4d1145dc1801ab9caf4a07
nguyenduc1222 <nguyenduc1222@gmail.com>
2020-06-06 15:29:56 +0700

    Update auto_scroll_and_like_facebook.js

--- auto_scroll_and_like_facebook.js
+++ auto_scroll_and_like_facebook.js
@@ -1,5 +1,5 @@
 setInterval(function() { window.scrollTo(0,document.body.scrollHeight); }, 60000);
 var countLike = 0;
 var countLikeClick = 0;
-setInterval(function() { if(!document.getElementsByClassName("_6a-y _3l2t  _18vj")[countLike].classList.contains("_3_16") ) {  console.log('check post ' + countLike); console.log('I was like ' + countLikeClick);  document.getElementsByClassName("_6a-y _3l2t  _18vj")[countLike].click(); countLikeClick++; } var _r = Math.floor(Math.random() * Math.floor(4)); countLike = countLike + 1 + _r; }, 30000);
+setInterval(function() { if(!document.getElementsByClassName("_6a-y _3l2t  _18vj")[countLike].classList.contains("_3_16") ) {  console.log('check post ' + countLike); console.log('I was like ' + countLikeClick);  document.getElementsByClassName("_6a-y _3l2t  _18vj")[countLike].click(); countLikeClick++; } var _r = Math.floor(Math.random() * Math.floor(2)); countLike = countLike + 1 + _r; }, 15000);
 
