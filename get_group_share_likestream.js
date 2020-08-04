39538cdc5224ebe56c4816da70a730fdd3b74f52
nguyenduc1222 <nguyenduc1222@gmail.com>
2020-08-05 06:58:30 +0700

    Update get_group_share_likestream.js

--- get_group_share_likestream.js
+++ get_group_share_likestream.js
@@ -1,1 +1,24 @@
-1
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var index = 0;
+
+while(true) {
+var listwpv = document.getElementsByClassName('_wpv');
+
+for (;index < listwpv.length; index++) {
+    var wpv = listwpv[index];
+    if (wpv.getAttribute('href')) {
+        var href = wpv.getAttribute('href');
+        var regex = /\/groups\/(\d+)\//;
+        var match = href.match(regex);
+        if (match) {
+            var idGroup = href.match(regex)[1];
+            console.log(idGroup);
+        }
+    } 
+}
+window.scrollTo(0,document.getElementById('repost_view_dialog').offsetHeight);
+await sleep(2000);
+}
